31b7902301b644d6eadad1aa35fc02d1f33b2fcc
Gheorghe Tarcea <tarcea@gmail.com>
2020-10-06 16:30:28 +0200

    baner background picture added

---
+++ src/components/banner/banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./banner.css";
+
+
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="b-center">
+          <p>Plan everithing of your journey here!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
