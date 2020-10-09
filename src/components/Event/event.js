e6c09ab96368692e62411470faaa9bf010ffc5ef
Gheorghe Tarcea <tarcea@gmail.com>
2020-10-09 11:48:05 +0200

    event card ready

--- src/components/Event/event.js
+++ src/components/Event/event.js
@@ -1,114 +1,70 @@
 import React from 'react';
+import { IconContext } from "react-icons";
+import { IoIosShareAlt, IoIosHeart } from 'react-icons/io';
+import { eventsData } from '../../utils/data';
 import './event.css';
 
 const Event = () => {
   return (
+
     <div className="event-container">
       <div className="left-scene">
       </div>
       <div className="right-scene">
-      Events
         <div className="parent">
-          <div className="event-card">
-            <a href="www">
-              <div className="event-img event-img-1">
-                <div className="price centralized">
-                  <p>Free</p>
-                </div>
-                <div className="share-like">
-                  <div className="share-hotel small-white-circle centralized">
-                    <i className="fas fa-share"></i>
+          {eventsData.map((event) => {
+            const styles = {backgroundImage: `url(${event.imgPath})`};
+            return (
+              <div className="event-card" key={event.id}>
+              <a href="www">
+                <div className="event-img" style={styles}>
+                  <div className="price centralized">
+                    <p>{event.price}</p>
                   </div>
-                  <div className="like-rate small-white-circle centralized">
-                    <i className="fas fa-heart"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="event-infor">
-                <p className="week">Monday</p>
-                <p className="title">The best Christmas Market</p>
-                <div className="calendar-frame">
-                  <span className="day">3</span>
-                  <span className="month">Sep</span>
-                </div>
-                  <p className="description">Lorem ipsum dolor sit amet, affert iracundia eam ad. No aeterno placerat.</p>
-                  <div className="btns-rows">
-                    <div className="btn-design">Design</div>
-                    <div className="btn-dance">Dance</div>
-                    <div className="btn-show">Shows</div>
-                  </div>
-              </div>
-            </a>
-          </div>
-
-        <div className="event-card">
-            <a href="www">
-              <div className="event-img event-img-2">
-                <div className="price centralized">
-                  <p>Free</p>
-                </div>
-                <div className="share-like">
-                  <div className="share-hotel small-white-circle centralized">
-                    <i className="fas fa-share"></i>
-                  </div>
-                  <div className="like-rate small-white-circle centralized">
-                    <i className="fas fa-heart"></i>
+                  <div className="share-like">
+                    <div className="share-hotel small-white-circle centralized">
+                      <IconContext.Provider value={{ color: "#F24B6A", className: "global-class-name"}}>
+                        <div>
+                          <IoIosShareAlt />
+                        </div>
+                      </IconContext.Provider>
+                    </div>
+                    <div className="like-rate small-white-circle centralized">
+                      <IconContext.Provider value={{ color: "#F24B6A", className: "global-class-name"}}>
+                        <div>
+                          <IoIosHeart />
+                        </div>
+                      </IconContext.Provider>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="event-infor">
-                <p className="week">Monday</p>
-                <p className="title">The best Christmas Market</p>
-                <div className="calendar-frame">
-                  <span className="day">3</span>
-                  <span className="month">Sep</span>
-                </div>
-                  <p className="description">Lorem ipsum dolor sit amet, affert iracundia eam ad. No aeterno placerat.</p>
-                  <div className="btns-rows">
-                    <div className="btn-design">Design</div>
-                    <div className="btn-dance">Dance</div>
-                    <div className="btn-show">Shows</div>
+                <div className="event-infor">
+                  <p className="week">{event.date.day}</p>
+                  <p className="title">{event.info.title}</p>
+                  <div className="calendar-frame">
+                    <span className="day">{event.date.date}</span>
+                    <span className="month">{event.date.month}</span>
                   </div>
-              </div>
-            </a>
-          </div>
-
-        <div className="event-card">
-            <a href="www">
-              <div className="event-img event-img-3">
-                <div className="price centralized">
-                  <p>Free</p>
+                    <p className="description">{event.info.description}</p>
+                    <div className="btns-rows">
+                      {event.categories.map((category) => {
+                          return (
+                        <fragment>
+                          <div className="btn-design">{category}</div>
+                        </fragment>
+                      );
+                      })}
+                    </div>
                 </div>
-                <div className="share-like">
-                  <div className="share-hotel small-white-circle centralized">
-                    <i className="fas fa-share"></i>
-                  </div>
-                  <div className="like-rate small-white-circle centralized">
-                    <i className="fas fa-heart"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="event-infor">
-                <p className="week">Monday</p>
-                <p className="title">The best Christmas Market</p>
-                <div className="calendar-frame">
-                  <span className="day">3</span>
-                  <span className="month">Sep</span>
-                </div>
-                  <p className="description">Lorem ipsum dolor sit amet, affert iracundia eam ad. No aeterno placerat.</p>
-                  <div className="btns-rows">
-                    <div className="btn-design">Design</div>
-                    <div className="btn-dance">Dance</div>
-                    <div className="btn-show">Shows</div>
-                  </div>
-              </div>
-            </a>
-          </div>
-
+              </a>
+            </div>
+          );
+          })}
         </div>
       </div>
     </div>
   );
+
 }
 
 export default Event;
---
+++ src/utils/data.js
@@ -0,0 +1,118 @@
+
+
+export const eventsData = [
+  {
+    id: 1,
+    price: "Free",
+    likes: 15000,
+    liked: false,
+    shared: false,
+    imgPath: "/images/event-found-1.png",
+    info: {
+      title: "The Best Christmas Market",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore",
+    },
+    categories: ['design', 'dance', 'shows'],
+    date: {
+      year: 2020,
+      month: 'Dec',
+      date: 20,
+      day: 'Sunday'
+    }
+  },
+  {
+    id: 2,
+    price: "€13",
+    likes: 150,
+    liked: true,
+    shared: false,
+    imgPath: "/images/event-found-2.png",
+    info: {
+      title: "Disco party",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore",
+    },
+    categories: ['music', 'dance', 'shows'],
+    date: {
+      year: 2020,
+      month: 'Oct',
+      date: 16,
+      day: 'Friday'
+    }
+  },
+  {
+    id: 3,
+    price: "€25",
+    likes: 2000,
+    liked: false,
+    shared: true,
+    imgPath: "/images/event-found-3.png",
+    info: {
+      title: "Long Night of Museums",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore",
+    },
+    categories: ['museum', 'architecture', 'exhibitions'],
+    date: {
+      year: 2020,
+      month: 'Dec',
+      date: 20,
+      day: 'Sunday'
+    }
+  },
+  {
+    id: 4,
+    price: "€25",
+    likes: 2000,
+    liked: false,
+    shared: true,
+    imgPath: "/images/event-found-3.png",
+    info: {
+      title: "Long Night of Museums",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore",
+    },
+    categories: ['museum', 'architecture', 'exhibitions'],
+    date: {
+      year: 2020,
+      month: 'Dec',
+      date: 20,
+      day: 'Sunday'
+    }
+  },
+  {
+    id: 5,
+    price: "€25",
+    likes: 2000,
+    liked: false,
+    shared: true,
+    imgPath: "/images/event-found-2.png",
+    info: {
+      title: "Long Night of Museums",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore",
+    },
+    categories: ['museum', 'architecture', 'exhibitions'],
+    date: {
+      year: 2020,
+      month: 'Dec',
+      date: 20,
+      day: 'Sunday'
+    }
+  },
+  {
+    id: 6,
+    price: "€25",
+    likes: 2000,
+    liked: false,
+    shared: true,
+    imgPath: "/images/event-found-1.png",
+    info: {
+      title: "Long Night of Museums",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore ",
+    },
+    categories: ['museum', 'architecture', 'exhibitions'],
+    date: {
+      year: 2020,
+      month: 'Dec',
+      date: 20,
+      day: 'Sunday'
+    }
+  },
+  ]
